144d36d55629f980b23ec52b177b0ce60dbea9cd
abhinaya-r <abhinaya.raghu@gmail.com>
2021-05-03 17:31:46 -0400

    added fog, larger trees

--- src/components/lights/BasicLights.js
+++ src/components/lights/BasicLights.js
@@ -5,14 +5,16 @@ class BasicLights extends Group {
         // Invoke parent Group() constructor with our args
         super(...args);
 
-        const dir = new SpotLight(0xffffff, 1.6, 7, 0.8, 1, 1);
+        const dir = new SpotLight(0xffffff);
+        dir.position.set(-40, 50, -40)
         const ambi = new AmbientLight(0x404040, 1.32);
-        const hemi = new HemisphereLight(0xffffbb, 0x080820, 2.3);
+        dir.castShadow = true;
+        const hemi = new HemisphereLight(0xffffbb, 0x136d16, 2.3);
 
-        dir.position.set(5, 1, 2);
-        dir.target.position.set(0, 0, 0);
+        // dir.position.set(5, 1, 2);
+        // dir.target.position.set(0, 0, 0);
 
-        this.add(ambi, hemi, dir);
+        this.add(dir, hemi );
     }
 }
 
--- src/components/scenes/SeedScene.js
+++ src/components/scenes/SeedScene.js
@@ -1,36 +1,110 @@
-import * as Dat from 'dat.gui';
-import { Scene, Color } from 'three';
-import { Flower, Land } from 'objects';
-import { BasicLights } from 'lights';
+import * as Dat from "dat.gui";
+import { Scene, Color, Fog } from "three";
+import { Flower, Land, Bear, Deer, PineTree, OakTree, ElmTree } from "objects";
+import { BasicLights } from "lights";
+import * as THREE from "three";
+// import { PineTree } from "../objects/PineTree";
+// import Scene from "../../../coursejs/scene.js";
 
 class SeedScene extends Scene {
-    constructor() {
-        // Call parent Scene() constructor
-        super();
+  constructor() {
+    // Call parent Scene() constructor
+    super();
 
-        // Init state
-        this.state = {
-            gui: new Dat.GUI(), // Create GUI for scene
-            rotationSpeed: 1,
-            updateList: [],
-        };
+    // Init state
+    this.state = {
+      gui: new Dat.GUI(), // Create GUI for scene
+      rotationSpeed: 0,
+      updateList: [],
+    };
 
-        // Set background to a nice color
-        this.background = new Color(0x7ec0ee);
+    // Set background to a nice color
+    this.background = new Color(0xb4cede);
+    this.fog = new Fog(0xb4cede, 40, 60);
 
-        // Add meshes to scene
-        const land = new Land();
-        const flower = new Flower(this);
-        const lights = new BasicLights();
-        this.add(land, flower, lights);
+    let ground = {};
+    ground.textures = {};
 
-        // Populate GUI
-        this.state.gui.add(this.state, 'rotationSpeed', -5, 5);
+    // ground material
+    ground.material = new THREE.MeshStandardMaterial({
+      color: 0x195228, //0x3c3c3c,
+      // specular: 0x404761, //0x3c3c3c//,
+      metalness: 0.3,
+    });
+
+    // ground mesh
+    ground.geometry = new THREE.PlaneBufferGeometry(20000, 20000);
+    ground.mesh = new THREE.Mesh(ground.geometry, ground.material);
+    ground.mesh.position.y = 0; // -249 - 1;
+    ground.mesh.rotation.x = -Math.PI / 2;
+    ground.mesh.receiveShadow = true;
+    this.add(ground.mesh); // add ground to scene
+
+    const lights = new BasicLights();
+    this.add(lights);
+
+    // DEER
+    let allDeer = [];
+    for (let i = 0; i < 100; i++) {
+      let speed = Math.random() * 0.3;
+      let visibleTime = Math.floor(Math.random() * 10000);
+      let stopTime = visibleTime + Math.floor(Math.random() * 100);
+      let x = Infinity;
+      let z = Infinity;
+      let deer = new Deer(this, x, z, speed, visibleTime, stopTime);
+      this.add(deer);
+      allDeer.push(deer);
     }
 
-    addToUpdateList(object) {
-        this.state.updateList.push(object);
+    // BEAR
+    // let allBears = [];
+    // for (let i = 0; i < 100; i++) {
+    //   let speed = Math.random() * 0.1;
+    //   let visibleTime = Math.floor(Math.random() * 10000);
+    //   let stopTime = visibleTime + Math.floor(Math.random() * 100);
+    //   let x = Infinity;
+    //   let z = Infinity;
+    //   let bear = new Bear(this, x, z, speed, visibleTime, stopTime);
+    //   this.add(bear);
+    //   allBears.push(bear);
+    // }
+
+    const bear = new Bear(this);
+    // const deer = new Deer(this);
+    for (let i = 0; i < 30; i++) {
+        let pine = new PineTree(this);
+        let x = Math.random()*100-50;
+        let z = Math.random()*150-60
+        pine.position.set(x, 0, z)
+        this.add(pine);
     }
+    for (let i = 0; i < 30; i++) {
+        let oak = new OakTree(this);
+        let x = Math.random()*100-50;
+        let z = Math.random()*200-50
+        oak.position.set(x, 0, z)
+        this.add(oak);
+    }
+    for (let i = 0; i < 30; i++) {
+        let elm = new ElmTree(this);
+        let x = Math.random()*100-50;
+        let z = Math.random()*100-30
+        elm.position.set(x, 0, z)
+        this.add(elm);
+    }
+    this.add(bear);
+
+    // Populate GUI
+    // this.state.gui.add(this.state, "rotationSpeed", -5, 5);
+  }
+
+  addToUpdateList(object) {
+    this.state.updateList.push(object);
+  }
+
+  // update(timeStamp) {
+  //   const { rotationSpeed, updateList } = this.state;
+  //   this.rotation.y = (rotationSpeed * timeStamp) / 10000;
 
     update(timeStamp) {
         const { rotationSpeed, updateList } = this.state;
@@ -42,5 +116,6 @@ class SeedScene extends Scene {
         }
     }
 }
+
 
 export default SeedScene;
