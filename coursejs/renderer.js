4a3daa0bbdb081012609ef04664c096541bc8ab2
Al Liang <alliang@dynamic-oit-visitornet101-10-24-108-49.princeton.org>
2021-05-04 21:51:24 -0400

    fix

--- coursejs/renderer.js
+++ coursejs/renderer.js
@@ -201,50 +201,50 @@ window.addEventListener("keyup", function(event) {
   // Ignore keypresses typed into a text box
   if (event.target.tagName == "INPUT") return;
 
-  if(event.key == "a") {
-    //canvas.restore();
-    if(inAlbum) {
-      // close the album
-    }
-    else {
-      //console.log(album);
-      if(album.length > 0) {
-        //album[currentPhoto].draw();
-      }
-    }
-  }
+  // if(event.key == "a") {
+  //   //canvas.restore();
+  //   if(inAlbum) {
+  //     // close the album
+  //   }
+  //   else {
+  //     //console.log(album);
+  //     if(album.length > 0) {
+  //       //album[currentPhoto].draw();
+  //     }
+  //   }
+  // }
 
-  if(event.key = "ArrowLeft") {
-    if(inAlbum) {
-      if(currentPhoto > 0) {
-        //album[--currentPhoto].draw();
-      }
-      else {
-        //album[album.length - 1].draw();
-        //currentPhoto = album.length -1;
-      }
-    }
-  }
+  // if(event.key = "ArrowLeft") {
+  //   if(inAlbum) {
+  //     if(currentPhoto > 0) {
+  //       //album[--currentPhoto].draw();
+  //     }
+  //     else {
+  //       //album[album.length - 1].draw();
+  //       //currentPhoto = album.length -1;
+  //     }
+  //   }
+  // }
 
-  if(event.key = "ArrowRight") {
-    if(inAlbum) {
-      if(currentPhoto < album.length - 1) {
-        //album[++currentPhoto].draw();
-      }
-      else {
-        //album[0].draw();
-        //currentPhoto = 0;
-      }
-    }
-  }
+  // if(event.key = "ArrowRight") {
+  //   if(inAlbum) {
+  //     if(currentPhoto < album.length - 1) {
+  //       //album[++currentPhoto].draw();
+  //     }
+  //     else {
+  //       //album[0].draw();
+  //       //currentPhoto = 0;
+  //     }
+  //   }
+  // }
 
   // if 'I' was released, download the image
   if (event.key == "i") {
-    document.getElementById("recIcon").classList.add("Rec");
-    document.getElementById("recIcon").classList.remove("notRec");
-    setTimeout(() => {Renderer.photo();
-                      document.getElementById("recIcon").classList.add("notRec");
-                      document.getElementById("recIcon").classList.remove("Rec");},1000);
+    // document.getElementById("recIcon").classList.add("Rec");
+    // document.getElementById("recIcon").classList.remove("notRec");
+    // setTimeout(() => {Renderer.photo();
+    //                   document.getElementById("recIcon").classList.add("notRec");
+    //                   document.getElementById("recIcon").classList.remove("Rec");},1000);
   }
 
   // If 'V' was released, stop recording,
